2386eb437912f5ba82eab04b0c33eae41dd67c4e
adityaK006 <120311231+adityaK006@users.noreply.github.com>
2025-06-02 00:41:22 +0530

    Updated port app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def index():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True) //updated the port
+
